15872256b3976cef3607e1cc1c00798960a47855
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-01-28 23:22:39 +0000

    replace express absolute Path with the relative Path

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ app.use('/src', express.static(__dirname + '/src'));
 app.use('/style', express.static(__dirname + '/style'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-app.get('/dist', function (req, res) {
-    res.sendFile('/dev/socialinvestment/dist');
-});
-
 app.get('*', function (req, res) {
-    res.sendFile('/dev/socialinvestment/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
-
-
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
