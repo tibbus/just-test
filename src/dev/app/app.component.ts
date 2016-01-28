a5b771218645cf817f9875667f29f96f67efba26
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-01-28 20:43:53 +0000

    add angular and styles

---
+++ src/dev/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>{{ sayHello }}</h1>`
+})
+
+export class AppComponent {
+
+    public sayHello: string = "Hello there !";
+}
