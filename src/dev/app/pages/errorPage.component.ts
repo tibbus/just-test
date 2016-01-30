98632b0ea437d703956a3f11afea6fb718e4c14b
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-01-30 18:08:32 +0000

    handle wrong route Name in the Error Page Component

--- src/dev/app/pages/errorPage.component.ts
+++ src/dev/app/pages/errorPage.component.ts
@@ -1,10 +1,16 @@
 ﻿import {Component} from 'angular2/core';
+import {Location} from 'angular2/router';
 
 @Component({
     selector: 'page-two-component',
-    template: `<h2>This page does not exists !</h2>    
+    template: `<h2>The Page <span style="color:red">{{ pageName }}</span> does not exists !</h2>    
              `
 })
 
 export class ErrorPage {
+    public pageName: String;
+
+    constructor(location: Location) {
+        this.pageName = location.path().substring(1);
+    }
 }
