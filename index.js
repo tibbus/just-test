fd13b42c3b8cda29ba1ba2921de10f69fa29aa03
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-03-06 11:53:36 +0000

    add website password

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var basicAuth = require('basic-auth-connect');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use('/src', express.static(__dirname + '/src'));
 app.use('/style', express.static(__dirname + '/style'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.use(basicAuth('bizcarapp', 'meetbiz550'));
 
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/index.html');
