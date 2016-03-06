ce98319f414ce8ed23591a68b8392a510cf66f89
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-03-06 18:47:19 +0000

    fix material design issues, refactor leftBar menu

--- src/dev/app/leftBar/leftBar.component.ts
+++ src/dev/app/leftBar/leftBar.component.ts
@@ -2,10 +2,17 @@
 
 @Component({
     selector: 'left-bar',
-    styles: [`.left-buttons {width: 100%}`],
+    styleUrls: ['src/dist/app/leftBar/leftBar.component.css'],
     templateUrl: 'src/dev/app/leftBar/leftBar.component.html'
 })
 
 export class LeftBarComponent {
-    public sayHello: string = "Hello there !";
+    private sayHello: string = "Hello there !";
+    private cars: string[] = ['All Cars', 'McLaren MP4-12C', 'Aston Martin DB9', 'Audi Q7'];
+    private docs: string[] = ['My Documents', 'Shared Folder', 'Public Library'];
+    private selected: string = this.cars[0];
+
+    onSelect(item: string): void {
+        this.selected = item;
+    }
 }
