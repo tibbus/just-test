c04328c37103e91a6f6e48a5a99fe4c352b5df72
Tiberiu Popescu <tiberiu.popescu@bedegaming.com>
2016-03-07 01:05:44 +0000

    update angular2 to latest beta.0 version
    * update angular2 dependencies libraries
    * add a fix for angular2 typings errors

--- src/dev/boot.ts
+++ src/dev/boot.ts
@@ -1,3 +1,5 @@
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
 import {bootstrap}        from 'angular2/platform/browser';
 import {AppComponent}     from './app/app.component';
 import {ROUTER_PROVIDERS} from 'angular2/router';
