a6cebbbeca6f2a8029ceb68cdb4c16ca8da3bc34
Tiberiu Popescu <Tiberiu Popescu>
2016-06-07 17:53:20 +0100

    make timeline a generic component, re-style

--- src/dev/app/common/timeline/timeline.component.ts
+++ src/dev/app/common/timeline/timeline.component.ts
@@ -14,10 +14,64 @@ import { EditModalComponent } from './editModal/editModal.component';
 })
 
 export class TimelineComponent {
+    private modalSubscription: Subscription;
+
+    statuses: any[];
+    loading: string;
+    modalName: string;
+    selectedPostId: string;
+
     constructor(
-        private _statusService: StatusService,
-        private _modalService: ModalService,
-        private _ref: ChangeDetectorRef,
-        private _timelineService: TimelineService
-    ) { }
+        private statusService: StatusService,
+        private modalService: ModalService,
+        private ref: ChangeDetectorRef,
+        private timelineService: TimelineService
+    ) {
+        //super(statusService, modalService, ref, timelineService);
+
+        // on modal open/close :
+        this.modalSubscription = modalService.modalName.subscribe(
+            modalName => {
+                this.modalName = modalName;
+
+                this.ref.detectChanges();
+
+                // open the modal
+                jQuery('#myModal').modal('show');
+            });
+    }
+
+    ngOnDestroy() {
+        this.modalSubscription.unsubscribe();
+    }
+
+    ngOnInit() {
+        this.timelineService.getPosts().subscribe(
+            (statuses: any) => {
+                this.statuses = statuses.results.reverse().map(item => {
+                    return item.details;
+                });
+            }
+        )
+    }
+
+    onClickDelete(statusId: string) {
+        this.loading = statusId;
+
+        this.statusService.deleteStatus(statusId).delay(500).subscribe(
+            statuses => {
+                this.loading = null;
+
+                // update the status list (make a new server request in the service)
+                this.timelineService.getPosts(true);
+            }
+        )
+    }
+
+    onClickEdit(postId: string) {
+        this.selectedPostId = postId;
+        this.timelineService.selectedPostId = postId;
+
+        this.modalService.setModalName('editModal');
+    }
 }
