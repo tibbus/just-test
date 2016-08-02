074822849f326fd29d2e27dd3403a972f50e055f
Tiberiu Popescu <Tiberiu Popescu>
2016-08-02 18:23:37 +0300

    add typings command to npm script

--- src/dev/boot.ts
+++ src/dev/boot.ts
@@ -3,15 +3,11 @@
 import 'rxjs/Rx';
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { provide } from '@angular/core';
-
-
 import { HTTP_PROVIDERS, RequestOptions } from '@angular/http';
 import { enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
-
 import { appRouterProviders }   from './app/app.routes';
-
 import { CustomRequestOptions } from './app/services/http/baseRequest';
 import {
     ApiService,
