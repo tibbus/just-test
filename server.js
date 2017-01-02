4d11ca033125d67a4252e6b502338308c2b398ed
Tiberiu Popescu <ttiberiup@gmail.com>
2017-01-02 13:53:14 +0100

    add the logger back and fix fake services

--- server.js
+++ server.js
@@ -7,28 +7,22 @@ const basicAuth = require('basic-auth-connect');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/style', express.static(__dirname + '/style'));
-app.use('/assets', express.static(__dirname + '/assets'));
 // used for ts maps
-app.use('/src', express.static(__dirname + '/src'));
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
-//app.use('/api', express.static(__dirname + '/api'));
-app.use('/systemjs.config.js', express.static(__dirname + '/systemjs.config.js'));
-app.use('/le.min.js', express.static(__dirname + '/le.min.js'));
-app.use('/oidc-client.js', express.static(__dirname + '/oidc-client.js'));
+//app.use('/src', express.static(__dirname + '/src'));
 
-// FAKE Service APIs :    
-app.use('/api/v1/user/1', express.static(__dirname + '/server/fakeService/profile.json'));
-app.use('/api/v1/user/1/usercar/details=true', express.static(__dirname + '/server/fakeService/userCars.json'));
+// FAKE Service APIs :
+app.use('/api/v1/user/77', express.static(__dirname + '/server/fakeService/profile.json'));
+app.use('/api/v1/user/77/usercar/details=true', express.static(__dirname + '/server/fakeService/userCars.json'));
 app.use('/search', express.static(__dirname + '/server/fakeService/search.json'));
 
 app.use('/callback.html', express.static(__dirname + '/callback.html'));
 
 app.use(basicAuth('bizcarapp', 'meetbiz550'));
 
+app.use('/', express.static(__dirname + '/dist'));
+
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(app.get('port'), () => {
