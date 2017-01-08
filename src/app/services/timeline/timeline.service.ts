406b4e17303788ed6f65732792bb657296c8966a
Tiberiu Popescu <ttiberiup@gmail.com>
2017-01-08 22:30:33 +0100

    remove unneeded car api requests

--- src/app/services/timeline/timeline.service.ts
+++ src/app/services/timeline/timeline.service.ts
@@ -3,21 +3,15 @@ import { Http } from '@angular/http';
 import { Subject } from 'rxjs/Subject';
 import * as _ from 'lodash';
 
-//import { ApiService, CarService, API, HttpService, Actor } from '../index';
 import { StreamService } from '../stream/stream.service';
 import { Timeline, Post } from './timeline.model';
-import { ApiService } from '../api/api.service';
 import { CarService } from '../car/car.service';
-import { API } from '../api/api';
-import { HttpService } from '../http/http.service';
 import { Actor } from '../stream/stream.model';
 
 
 @Injectable()
 export class TimelineService {
     constructor(
-        private http: Http,
-        private apiService: ApiService,
         private carService: CarService,
         private streamService: StreamService
     ) {}
@@ -31,17 +25,9 @@ export class TimelineService {
     public getPosts() {
        return this.streamService.getData(this.actor, 'get').do((posts: any[]) => {
             this.posts = posts.map(post => {
-                const carInfoUrl: string = this.apiService.getCarInfoUrl(post.activityData.carInfoId);
-
                 post.comments = {
                     state: '+'
                 };
-
-                this.http.get(carInfoUrl).map((res: any) => {
-                    return res.json();
-                }).subscribe(car => {
-                    post.carInfo = car.carInfo.car;
-                });
 
                 return post;
             });
