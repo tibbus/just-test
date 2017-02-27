5837bfc640463ded5d57c3dd8612fd12d118885c
Tiberiu Popescu <ttiberiup@gmail.com>
2017-02-27 20:06:58 +0100

    remove unused module to fix the build

--- src/app/views/+content/car/carTimeline/carTimeline.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-
-import { ModalService, TimelineService } from '../../../../services/index';
-import { TimelineComponent } from '../../../../shared/timeline/timeline.component'; 
-
-export class CarTimelineComponent extends TimelineComponent {
-    
-}
