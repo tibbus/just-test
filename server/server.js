2f741ddbc5b9242cd230917efdb2e1854f23b005
Tiberiu Popescu <ttiberiup@gmail.com>
2017-03-28 09:36:07 +0300

    fix assets absolute path

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ const basicAuth = require('basic-auth-connect');
 
 app.set('port', (process.env.PORT || 5000));
 
-// used for ts maps
-//app.use('/src', express.static(__dirname + '/src'));
+app.use('/src/assets', express.static('../src/assets'));
 
 // FAKE Service APIs :
 app.use('/api/v1/user/77', express.static('./fakeService/profile.json'));
