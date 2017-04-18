2d85ba139561958f16d5981179f1920aac74c349
Tiberiu Popescu <ttiberiup@gmail.com>
2017-04-18 11:17:56 +0300

    update ng cli config to latest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './polyfills.ts';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
 
-import 'rxjs/Rx';
+import './polyfills';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
